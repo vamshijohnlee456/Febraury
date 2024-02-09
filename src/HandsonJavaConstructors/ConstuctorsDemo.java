a244899f2865f7894fd89daa8148edf55cab6ae3
vamshijohnlee456 <lingampellyvamshi456@gmail.com>
2024-02-09 16:10:33 +0530

    commit new code

--- src/HandsonJavaConstructors/ConstuctorsDemo.java
+++ src/HandsonJavaConstructors/ConstuctorsDemo.java
@@ -5,7 +5,7 @@ public class ConstuctorsDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hy welcome to java");
-
+		System.out.println("hy welcome to java");
 	}
 
 }
